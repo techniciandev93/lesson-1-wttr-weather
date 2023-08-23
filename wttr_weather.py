0236e4019d8ef5fb5f9d76abbc0a251e8fabead3
anton <a.solovjev@vzv.su>
2023-08-23 13:12:24 +0300

    added changes

--- wttr_weather.py
+++ wttr_weather.py
@@ -1,28 +1,31 @@
 import requests
 
 
-def send_request(url):
-    response = requests.get(url)
-    return response.text
+def send_request(url, params):
+    response = requests.get(url, params=params)
+    if response.ok:
+        return response.text
+    else:
+        response.raise_for_status()
 
 
-def wttr_weather_main():
-    url = 'https://wttr.in/{}{}'
-
-    dict_cities = {
-        'Лондон': {'path': 'london', 'params': '?lang=ru&MnqT'},
-        'Аэропорт Шереметьево': {'path': 'cvo', 'params': '?lang=ru&MnqT'},
-        'Череповц': {'path': 'Череповец', 'params': '?lang=ru&MnqT'}
-    }
-
-    for city in dict_cities:
-        path = dict_cities[city].get('path')
-        params = dict_cities[city].get('params')
-        params = params if params else ''
-        full_url = url.format(path, params)
-        resp_text = send_request(full_url)
-        print(resp_text)
+def create_url(data_city, url):
+    path = data_city.get('path')
+    ready_params = data_city.get('params')
+    ready_url = url.format(path)
+    return ready_url, ready_params
 
 
 if __name__ == '__main__':
-    wttr_weather_main()
+    url_wttr = 'https://wttr.in/{}'
+
+    cities = {
+        'Лондон': {'path': 'london', 'params': {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': ''}},
+        'Аэропорт Шереметьево': {'path': 'CVO', 'params': {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': ''}},
+        'Череповц': {'path': 'Череповец', 'params': {'lang': 'ru', 'M': '', 'n': '', 'q': '', 'T': ''}}
+    }
+
+    for city in cities:
+        full_url, params_city = create_url(cities[city], url_wttr)
+        resp_text = send_request(full_url, params_city)
+        print(resp_text)
